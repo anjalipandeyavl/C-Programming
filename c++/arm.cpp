2efefd721d8f4ffc4dedcec0f346e8dc09e176b6
anjalipandeyavl <priyanjali835@gmail.com>
2022-07-23 15:11:41 +0530

    gj

--- c++/arm.cpp
+++ c++/arm.cpp
@@ -1,31 +1,29 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
-int num,rem,sum=0,temp,temp1,count=0;
-cout<<"enter number to check : ";
-cin>>num;
-temp=num;
-temp1=num;
+    int num, rem, sum = 0, temp, temp1, count = 0;
+    cout << "enter number to c heck : ";
+    cin >> num;
+    temp = num;
+    temp1 = num;
 
-while(num>0)
-{
-num = num/10;
-count++;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
 
-}
+    while (temp > 0)
+    {
+        rem = temp % 10;
+        sum = sum + pow(rem, count);
+        temp = temp / 10;
+    }
 
-while(temp>0)
-{
-rem=temp%10;
-sum=sum+pow(rem,count);
-temp=temp/10;
-
-}
-
-if(temp1==sum)
-cout<<"Armstrong ";
-else
-cout<<"Not armstong ";
+    if (temp1 == sum)
+        cout << "Armstrong ";
+    else
+        cout << "Not armstong ";
 }
