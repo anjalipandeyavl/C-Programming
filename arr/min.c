344ec5dc8e6279d163aa60816a95e1a16817c7e2
anjalipandeyavl <priyanjali835@gmail.com>
2022-08-01 11:05:43 +0530

    File

--- arr/min.c
+++ arr/min.c
@@ -1,23 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-int i,arr[50],size,min;
-printf("\enter size");
-scanf("%d",&size);
+    int i, arr[50], size, min;
+    printf("\enter size");
+    scanf("%d", &size);
 
-printf("\n enter array element \n");
-for(i=0;i<size;i++)
-{
-scanf("%d",&arr[i]);
-}
-min=arr[0];
-printf("\n array element are : \n");
-for(i=0;i<size;i++)
-{
-if(min>arr[i])
-min=arr[i];
-printf("%d\t",arr[i]);
-}
-printf("\nminimum element  : %d\n",min);
-
+    printf("\n enter array element \n");
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    min = arr[0];
+    printf("\n array element are : \n");
+    for (i = 0; i < size; i++)
+    {
+        if (min > arr[i])
+            min = arr[i];
+        printf("%d\t", arr[i]);
+    }
+    printf("\nminimum element  : %d\n", min);
 }
---
+++ newpro1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int r, c;
+
+    for (r = 0; r <= 5; r++)
+    {
+        for (c = 1; c <= 34; c++)
+        {
+            if ((c >= 6 - r && c < 12 + r) || (c >= 24 - r && c <= 30 + r))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    for (r = 0; r < 18; r++)
+    {
+        for (c = 1; c <= 34; c++)
+        {
+            if (c >= r + 1 && c <= 34 - r)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
